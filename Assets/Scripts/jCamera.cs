26c2f6a58e67e4a56cea898c66f54d6e20a6982b
potentialk0 <64459852+potentialk0@users.noreply.github.com>
2020-07-21 23:38:53 +0900

    movement, animation

---
+++ Assets/Scripts/jCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jCamera : MonoBehaviour
+{
+
+    public float rotSpeed = 1f;
+    public Transform target;
+    public Transform player;
+    float mouseX;
+    float mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ Assets/Scripts/jPlayer.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+using Unity;
+
+public class jPlayer : MonoBehaviour
+{
+    Animator anim;
+    CharacterController controller;
+
+    [SerializeField]
+    private float moveSpeed = 4f;
+    private float currentSpeed = 0f;
+    private float speedSmoothVelocity = 0f;
+    private float speedSmoothTime = 0.1f;
+    private float rotSpeed = 0.1f;
+    private float gravity = 8f;
+
+    private Transform mainCameraTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Animation();
+        Test();
+    }
+
+    void Test()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void Movement()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 forward = mainCameraTransform.forward;
+        Vector3 right = mainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 desiredMoveDir = (right * movementInput.x + forward * movementInput.y).normalized;
+        Vector3 gravityVec = Vector3.zero;
+
+        if (!controller.isGrounded)
+        {
+            gravityVec.y -= gravity;
+        }
+
+        float targetSpeed = moveSpeed * movementInput.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        controller.Move(desiredMoveDir * currentSpeed * Time.deltaTime);
+        controller.Move(gravityVec * Time.deltaTime);
+    }
+
+    private void Animation()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementY = Input.GetAxisRaw("Vertical");
+        if(movementY > 0)
+        {
+            anim.SetInteger("condition", 1);
+        }
+        if(movementY < 0)
+        {
+            anim.SetInteger("condition", 2);
+        }
+        if (movementX < 0)
+        {
+            anim.SetInteger("condition", 3);
+        }
+        if (movementX > 0)
+        {
+            anim.SetInteger("condition", 4);
+        }
+        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
+            anim.SetInteger("condition", 0);
+    }
+}
