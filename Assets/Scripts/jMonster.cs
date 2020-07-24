40527e6acb07dcbc29c9883520e7c25e7dad1a1f
Nope <trash@mail.com>
2020-07-24 15:47:37 +0900

    monster start

---
+++ Assets/Scripts/jMonster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jMonster : MonoBehaviour
+{
+    LayerMask _player;
+    Vector3 _rightView;
+    Vector3 _leftView;
+    public float _agroDist = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rightView = (Vector3.forward + Vector3.right).normalized * _agroDist;
+        _leftView = -_rightView;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MonsterAI();
+    }
+
+    void MonsterAI()
+    {
+        
+    }
+}
